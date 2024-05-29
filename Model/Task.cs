5f7463b7f2f5187c26d19bf0c2026fc867e01892
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 19:10:33 +0500

    Разработанные модели данных. Осокин

---
+++ Model/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ПР45_Осокин.Model
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Priority { get; set; }
+        public DateTime DateExecute { get; set; }
+        public string Comment { get; set; }
+        public bool Done { get; set; }
+    }
+}
