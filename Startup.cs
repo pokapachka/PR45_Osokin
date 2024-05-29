75dafaf5e66eaaec8fc84f211a38c5d6e2b6ea46
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 19:09:29 +0500

    Настройка проекта, подключение swagger. Осокин

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,24 +17,50 @@ namespace ПР45_Осокин
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Руководство для пользования запросов",
+                    Description = "Полное руководство для использования запросов находящихся в проекте"
+                });
+                c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "Руководство для пользования запросов",
+                    Description = "Полное руководство для использования запросов находящихся в проекте"
+                });
+                c.SwaggerDoc("v3", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v3",
+                    Title = "Руководство для пользования запросов",
+                    Description = "Полное руководство для использования запросов находящихся в проекте"
+                });
+                c.SwaggerDoc("v4", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v4",
+                    Title = "Руководство для пользования запросов",
+                    Description = "Полное руководство для использования запросов находящихся в проекте"
+                });
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "API_Galkin.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseMvcWithDefaultRoute();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Запросы GET");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "Запросы POST");
+                c.SwaggerEndpoint("/swagger/v3/swagger.json", "Запросы PUT");
+                c.SwaggerEndpoint("/swagger/v4/swagger.json", "Запросы DELETE");
             });
         }
     }
