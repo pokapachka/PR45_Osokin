babf27291baff94bb25fd02c23d0c8b6104f1633
pokapachka <pachkaosokin@yandex.ru>
2024-05-29 19:14:50 +0500

    Разработанный контекст для загрузки данных. Осокин

---
+++ Context/TaskContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using ПР45_Осокин.Model;
+
+namespace ПР45_Осокин.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Tasks> Tasks { get; set; }
+
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=localhost;" + "port=3306;" + "uid=root;" + "pwd=;" +"database=TaskManager",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+    }
+}
