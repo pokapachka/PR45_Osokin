275d007c47316b0d2c55c3a746cffaeebc051540
pokapachka <pachkaosokin@yandex.ru>
2024-06-02 13:45:17 +0500

    Создание модели и контекста пользователя. Осокин

---
+++ Context/UsersContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using ПР45_Осокин.Model;
+
+namespace ПР45_Осокин.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;" + "port=3306;" + "uid=root;" + "pwd=;" + "database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ Model/Users.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace ПР45_Осокин.Model
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
